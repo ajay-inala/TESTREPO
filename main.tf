77912d394ec65fbb3404e8c82ffcfb7e015a153e
ajay-inala <153492651+ajay-inala@users.noreply.github.com>
2024-07-23 17:09:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "google" {
 
 resource "google_storage_bucket" "Test-Bucket" {
   name          = "Test-bucket"
-  location      = "US"
+  location      = "us-central1
   force_destroy = true
 
   public_access_prevention = "enforced"
