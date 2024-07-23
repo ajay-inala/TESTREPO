02143f1273c8726e8f9a461f8e62811b130a2dc2
ajay-inala <153492651+ajay-inala@users.noreply.github.com>
2024-07-23 15:52:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "google" {
+  project     = "my-project-id"
+  region      = "us-central1"
+  zone        = "           "
+}
+
+
 resource "google_storage_bucket" "Test-Bucket" {
   name          = "Test-bucket"
   location      = "US"
