e1a9eb2563ba301ab86ff87de40b4ad0b5e6ff88
ajay-inala <153492651+ajay-inala@users.noreply.github.com>
2024-07-24 12:38:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "google" {
   project     = "Project-cicd-testing"
   region      = "us-central1"          
 }
-resource "google_storage_bucket" "test-bucket" {
-  name          = "inala-storage-bucket"
+resource "google_storage_bucket" "test-bucket2" {
+  name          = "inala-storage-bucket2"
   location      = "us-central1"
   force_destroy = true
   public_access_prevention = "enforced"
