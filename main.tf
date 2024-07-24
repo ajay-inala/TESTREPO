5c40750db3ea49ed851ce0711be3540b79d02165
ajay-inala <153492651+ajay-inala@users.noreply.github.com>
2024-07-24 12:37:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,22 +8,3 @@ resource "google_storage_bucket" "test-bucket" {
   force_destroy = true
   public_access_prevention = "enforced"
 }
-resource "google_service_account" "default" {
-account_id   = "106467782216096746723"
-display_name = "Custom SA for VM Instance"
-}
-resource "google_compute_instance" "terraform" {
-  name         = "terraform"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-network_interface {
-    network = "default"
-    access_config {
-    }
-}
-}
