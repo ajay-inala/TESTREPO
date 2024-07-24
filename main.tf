362bf9fa093fc37e3932fb7ad91187f32794e868
ajay-inala <153492651+ajay-inala@users.noreply.github.com>
2024-07-24 15:25:29 +0530

    Create main.tf
    add secrets

---
+++ main.tf
@@ -0,0 +1,26 @@
+variable "secrets" {
+   default     = "secrets.json"
+  }
+provider "google" {
+  project     = "Project-cicd-testing"
+  region      = "us-central1"
+  credentials = "${file("${var.secrets}")}"
+}
+resource "google_compute_instance" "terraform" {
+  name         = "terraform"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+ network_interface {
+    network = "default"
+    access_config {
+    }
+}
+ service_account {
+    scopes = ["233044675135-compute@developer.gserviceaccount.com"]
+  }
+}
