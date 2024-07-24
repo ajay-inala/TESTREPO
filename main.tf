a991973e97f9400ab939897e3ab572d4b56380fa
ajay-inala <153492651+ajay-inala@users.noreply.github.com>
2024-07-24 13:19:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,11 @@
 provider "google" {
   project     = "Project-cicd-testing"
   region      = "us-central1"         
-  credentials = file("secrets.json")
 }
 resource "google_storage_bucket" "test-bucket2" {
-  name          = "inala-storage-bucket2"
+  name          = "inala-storage-bucket3"
   location      = "us-central1"
   force_destroy = true
   public_access_prevention = "enforced"
-}
-resource "google_compute_instance" "terraform" {
-  name         = "terraform"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
- network_interface {
-    network = "default"
-    access_config {
-    }
-}
- service_account {
-    scopes = ["233044675135-compute@developer.gserviceaccount.com"]
-  }
 }
 
