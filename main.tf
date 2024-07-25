54f7f15ff0313846ee17eefe2691c48f305e145d
ajay-inala <153492651+ajay-inala@users.noreply.github.com>
2024-07-25 13:23:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "google" {
   project     = "Project-cicd-testing"
   region      = "us-central1"         
 }
-resource "google_storage_bucket" "test-bucket2" {
-  name          = "Divya-storage-bucket2"
+resource "google_storage_bucket" "test-bucket3" {
+  name          = "Divya-storage-bucket4"
   location      = "us-central1"
   force_destroy = true
   public_access_prevention = "enforced"
